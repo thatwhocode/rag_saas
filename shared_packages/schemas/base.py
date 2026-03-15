80596be56f81b4b78332f8b7e0bd064049c27ff1
thatwhocode <stasytkin0000@gmail.com>
2026-03-15 17:10:45 +0100

    chore: stop tracking pycache files

--- shared_packages/schemas/base.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-
-class CoreModel(BaseModel):
-    model_config = ConfigDict(
-        from_attributes=True,
-        str_strip_whitespace=True,
-        extra='ignore'
-    )
